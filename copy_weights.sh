60fc07e18d1a784f2cb219cbcf0c44766a182eb5
rob mathews <rob.mathews@goodmeasures.com>
2023-06-28 06:04:07 -0400

    oops

--- copy_weights.sh
+++ copy_weights.sh
@@ -1,8 +1,8 @@
-scp ../vicuna-weights/config.json minigpt2:MiniGPT-4/vicuna-weights
-scp ../vicuna-weights/generation_config.json minigpt2:MiniGPT-4/vicuna-weights
-scp ../vicuna-weights/pytorch_model.bin.index.json minigpt2:MiniGPT-4/vicuna-weights
-scp ../vicuna-weights/special_tokens_map.json minigpt2:MiniGPT-4/vicuna-weights
-scp ../vicuna-weights/tokenizer.model minigpt2:MiniGPT-4/vicuna-weights
-scp ../vicuna-weights/tokenizer_config.json minigpt2:MiniGPT-4/vicuna-weights
-scp ../vicuna-weights/pytorch_model-00001-of-00002.bin minigpt2:MiniGPT-4/vicuna-weights
-scp ../vicuna-weights/pytorch_model-00002-of-00002.bin minigpt2:MiniGPT-4/vicuna-weights
+scp ../vicuna-weights/config.json minigpt:MiniGPT-4/vicuna-weights
+scp ../vicuna-weights/generation_config.json minigpt:MiniGPT-4/vicuna-weights
+scp ../vicuna-weights/pytorch_model.bin.index.json minigpt:MiniGPT-4/vicuna-weights
+scp ../vicuna-weights/special_tokens_map.json minigpt:MiniGPT-4/vicuna-weights
+scp ../vicuna-weights/tokenizer.model minigpt:MiniGPT-4/vicuna-weights
+scp ../vicuna-weights/tokenizer_config.json minigpt:MiniGPT-4/vicuna-weights
+scp ../vicuna-weights/pytorch_model-00001-of-00002.bin minigpt:MiniGPT-4/vicuna-weights
+scp ../vicuna-weights/pytorch_model-00002-of-00002.bin minigpt:MiniGPT-4/vicuna-weights
